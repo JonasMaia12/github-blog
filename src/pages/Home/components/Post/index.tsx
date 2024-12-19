2df5f7231b53cd4212a7d6c50a3072a17c9cec99
JonasMaia12 <jonaspiresmaia7@gmail.com>
2024-12-19 19:12:03 +0000

    Add search and posts

---
+++ src/pages/Home/components/Post/index.tsx
@@ -0,0 +1,22 @@
+import { PostContainer } from "./styles"
+
+export function Post() {
+  return (
+    <PostContainer>
+      <div>
+        <strong>
+          JavaScript data types and data structures JavaScript data types and
+          data structures
+        </strong>
+        <span>Há 1 dia</span>
+      </div>
+      <p>
+        Aprenda sobre os principais tipos de dados em JavaScript, como strings,
+        números, boleanos, objetos e arrays. Aprenda sobre os principais tipos
+        de dados em JavaScript, como strings, números, boleanos, objetos e
+        arrays. Aprenda sobre os principais tipos de dados em JavaScript, como
+        strings, números, boleanos, objetos e arrays.
+      </p>
+    </PostContainer>
+  )
+}
