64e7bbbaa652ab88d1d63fd916a0b149cf3779fe
JonasMaia12 <jonaspiresmaia7@gmail.com>
2024-12-19 11:48:21 +0000

    first commit

---
+++ src/styles/theme/defaultTheme.ts
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+  colors: {
+    "base-input": "#040f1a",
+    "base-background": "#071422",
+    "base-profile": "#0b1b2b",
+    "base-post": "#112131",
+    "base-border": "#1c2f41",
+    "base-label": "#3a536b",
+    "base-span": "#7b96b2",
+    "base-text": "#afc2d4",
+    "base-subtitle": "#c4d4e3",
+    "base-title": "#e7edf4",
+    "brand-blue": "#3294f8",
+  },
+  textSizes: {
+    "title-l": "1.5rem",
+    "title-m": "1.25rem",
+    "title-s": "1.125rem",
+    "text-m": "1rem",
+    "text-s": "0.875rem",
+    "componente-link": "0.75rem",
+  },
+} as const
